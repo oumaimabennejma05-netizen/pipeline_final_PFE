b48d6b2cd09813108887b3b9a3a543652113ddb0
oumaimabennejma05-netizen <oumaimabennejma05@gmail.com>
2026-05-11 19:19:30 +0100

    update code

--- BD/gmao_backup_20260324_1458.sql
+++ BD/gmao_backup_20260324_1458.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
-\restrict AzD15abBh0htDkEoopO6EohD7u5KCVLEsI1YAlKx7YonpLTOhptdYmeQJWxg3tS
+\restrict OJHOqmueXdssILR9V2eBIUdXiA96FR4KAwH0v6FtjJKh9AreVe1BbBBbLIGbrmn
 
--- Dumped from database version 15.17 (Ubuntu 15.17-1.pgdg24.04+1)
--- Dumped by pg_dump version 15.17 (Ubuntu 15.17-1.pgdg24.04+1)
+-- Dumped from database version 16.13 (Ubuntu 16.13-0ubuntu0.24.04.1)
+-- Dumped by pg_dump version 16.13 (Ubuntu 16.13-0ubuntu0.24.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -32,7 +32,8 @@ CREATE TABLE public.machine_data (
     runtime double precision,
     temperature double precision,
     vibration double precision,
-    machine_id bigint NOT NULL
+    machine_id bigint NOT NULL,
+    tension double precision
 );
 
 
@@ -67,7 +68,7 @@ CREATE TABLE public.machines (
     model character varying(255),
     name character varying(255) NOT NULL,
     status character varying(255) NOT NULL,
-    CONSTRAINT machines_status_check CHECK (((status)::text = ANY ((ARRAY['OPERATIONAL'::character varying, 'MAINTENANCE'::character varying, 'BROKEN'::character varying, 'IDLE'::character varying])::text[])))
+    CONSTRAINT machines_status_check CHECK (((status)::text = ANY (ARRAY[('OPERATIONAL'::character varying)::text, ('MAINTENANCE'::character varying)::text, ('BROKEN'::character varying)::text, ('IDLE'::character varying)::text])))
 );
 
 
@@ -136,8 +137,9 @@ CREATE TABLE public.tasks (
     title character varying(255) NOT NULL,
     machine_id bigint,
     technician_id bigint,
-    CONSTRAINT tasks_priority_check CHECK (((priority)::text = ANY ((ARRAY['LOW'::character varying, 'MEDIUM'::character varying, 'HIGH'::character varying, 'CRITICAL'::character varying])::text[]))),
-    CONSTRAINT tasks_status_check CHECK (((status)::text = ANY ((ARRAY['PENDING'::character varying, 'IN_PROGRESS'::character varying, 'PENDING_APPROVAL'::character varying, 'COMPLETED'::character varying, 'APPROVED'::character varying, 'CANCELLED'::character varying])::text[])))
+    created_by_id bigint,
+    CONSTRAINT tasks_priority_check CHECK (((priority)::text = ANY (ARRAY[('LOW'::character varying)::text, ('MEDIUM'::character varying)::text, ('HIGH'::character varying)::text, ('CRITICAL'::character varying)::text]))),
+    CONSTRAINT tasks_status_check CHECK (((status)::text = ANY (ARRAY[('EN_ATTENTE'::character varying)::text, ('IN_PROGRESS'::character varying)::text, ('PENDING_APPROVAL'::character varying)::text, ('COMPLETED'::character varying)::text, ('APPROVED'::character varying)::text, ('CANCELLED'::character varying)::text])))
 );
 
 
@@ -169,14 +171,20 @@ CREATE TABLE public.users (
     address character varying(255),
     created_at timestamp(6) without time zone,
     email character varying(255) NOT NULL,
-    id_number character varying(255),
+    cin character varying(255),
     name character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
     profile_picture character varying(255),
     role character varying(255) NOT NULL,
     two_factor_enabled boolean NOT NULL,
     two_factor_secret character varying(255),
-    CONSTRAINT users_role_check CHECK (((role)::text = ANY ((ARRAY['ADMIN'::character varying, 'RESPONSABLE'::character varying, 'TECHNICIAN'::character varying])::text[])))
+    city character varying(255),
+    password_reset_token character varying(255),
+    password_reset_token_expiry timestamp(6) without time zone,
+    postal_code character varying(255),
+    uuid character varying(255),
+    must_change_password boolean DEFAULT false,
+    CONSTRAINT users_role_check CHECK (((role)::text = ANY (ARRAY[('ADMIN'::character varying)::text, ('RESPONSABLE'::character varying)::text, ('TECHNICIAN'::character varying)::text])))
 );
 
 
@@ -238,15 +246,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: machine_data; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.machine_data (id, created_at, runtime, temperature, vibration, machine_id) FROM stdin;
-1	2026-03-23 20:01:25.314503	950	87.5	3.2	1
-2	2026-03-23 20:01:25.318566	948	85	3	1
-3	2026-03-23 20:01:25.321028	320	55	2.1	2
-4	2026-03-23 20:01:25.323958	318	53	2	2
-5	2026-03-23 20:01:25.326595	1100	65	8.9	3
-6	2026-03-23 20:01:25.328692	1098	68	9.1	3
-7	2026-03-23 20:01:25.33105	820	72	4.8	4
-8	2026-03-23 20:01:25.333125	1350	95	7.5	5
+COPY public.machine_data (id, created_at, runtime, temperature, vibration, machine_id, tension) FROM stdin;
 \.
 
 
@@ -255,11 +255,9 @@ COPY public.machine_data (id, created_at, runtime, temperature, vibration, machi
 --
 
 COPY public.machines (id, created_at, location, maintenance_date, model, name, status) FROM stdin;
-1	2026-03-23 20:01:25.308196	Workshop A	2026-05-23	FANUC-30i	CNC Machine Alpha	OPERATIONAL
-2	2026-03-23 20:01:25.309486	Workshop B	2026-04-23	DMG-CLX500	Lathe Machine Beta	OPERATIONAL
-3	2026-03-23 20:01:25.310324	Press Room	2026-03-18	ENERPAC-P142	Hydraulic Press Gamma	MAINTENANCE
-4	2026-03-23 20:01:25.31106	Utility Room	2026-06-23	ATLAS-GA55	Air Compressor Delta	OPERATIONAL
-5	2026-03-23 20:01:25.311823	Assembly Line	2026-03-23	INTRALOX-T500	Conveyor Belt Epsilon	BROKEN
+8	2026-04-26 16:08:47.293754	Atelier A	2026-05-01	f30	vibrateur	OPERATIONAL
+9	2026-04-26 16:09:26.118612	Atelier B	2026-05-10	FANUC-30	Machine CNC	OPERATIONAL
+10	2026-04-26 16:10:08.869545	Atelier C	2026-05-03	lenovo	Imprimente	BROKEN
 \.
 
 
@@ -268,8 +266,6 @@ COPY public.machines (id, created_at, location, maintenance_date, model, name, s
 --
 
 COPY public.task_history (id, completed_at, notes, task_id, technician_id) FROM stdin;
-1	2026-03-23 20:31:53.468382	Task submitted for approval	1	3
-2	2026-03-23 20:32:23.522228	Task approved by responsable	1	2
 \.
 
 
@@ -277,12 +273,8 @@ COPY public.task_history (id, completed_at, notes, task_id, technician_id) FROM 
 -- Data for Name: tasks; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.tasks (id, created_at, description, due_date, priority, status, title, machine_id, technician_id) FROM stdin;
-2	2026-03-23 20:01:25.340572	Critical vibration detected. Inspect bearings, mounting, and hydraulic seals.	2026-03-23	CRITICAL	PENDING	Hydraulic Press Emergency Maintenance	3	4
-4	2026-03-23 20:01:25.34616	Perform monthly lubrication routine on DMG CLX500 lathe machine.	2026-03-20	LOW	COMPLETED	Lathe Monthly Lubrication	2	4
-5	2026-03-23 20:01:25.347615	Replace air filters on Atlas GA55 compressor. Runtime approaching maintenance threshold.	2026-03-30	MEDIUM	PENDING	Compressor Filter Replacement	4	3
-3	2026-03-23 20:01:25.344843	Conveyor belt has stopped. Diagnose fault and replace damaged components.	2026-03-23	CRITICAL	IN_PROGRESS	Conveyor Belt Repair	5	3
-1	2026-03-23 20:01:25.335488	Inspect and clean cooling system on CNC Machine Alpha. Temperature trending above warning threshold.	2026-03-25	HIGH	APPROVED	CNC Temperature Inspection	1	3
+COPY public.tasks (id, created_at, description, due_date, priority, status, title, machine_id, technician_id, created_by_id) FROM stdin;
+18	2026-04-26 16:11:50.266143	reparer l'imprimente	2026-05-10	MEDIUM	EN_ATTENTE	reparation	10	3	2
 \.
 
 
@@ -290,11 +282,10 @@ COPY public.tasks (id, created_at, description, due_date, priority, status, titl
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.users (id, address, created_at, email, id_number, name, password, profile_picture, role, two_factor_enabled, two_factor_secret) FROM stdin;
-1	123 Industrial Zone, Tunis	2026-03-23 20:01:25.277697	admin@gmao.com	ADM001	Admin GMAO	$2a$10$Qe82xiI/LDL3DLmFxrkrG.KHX6LMKmtm86bhqF6apuRf0sCx//VAa	\N	ADMIN	f	\N
-2	456 Factory St, Sfax	2026-03-23 20:01:25.300351	responsable@gmao.com	RSP001	Mohamed Ali	$2a$10$HYmqTuzwLdO3.QOOrYSSuOUneBo6dfvc89uVdJMTo4ETHLNoZpKsq	\N	RESPONSABLE	f	\N
-3	789 Workshop Ave, Sousse	2026-03-23 20:01:25.301449	tech1@gmao.com	TCH001	Karim Sassi	$2a$10$Ubljmy05tOEKrdKGDVOF/eCfqlEbQa6dAvGZQP2yUlELtyU3W1i3m	\N	TECHNICIAN	f	\N
-4	321 Maintenance Rd, Monastir	2026-03-23 20:01:25.302539	tech2@gmao.com	TCH002	Amira Ben Salah	$2a$10$NuDI9iwiQ6IMmAgQsuyxeuATrQVfHzai/BzkvN2Bi4HsBBab9RVsu	\N	TECHNICIAN	f	\N
+COPY public.users (id, address, created_at, email, cin, name, password, profile_picture, role, two_factor_enabled, two_factor_secret, city, password_reset_token, password_reset_token_expiry, postal_code, uuid, must_change_password) FROM stdin;
+3	789 Workshop Ave, Sousse	2026-03-23 20:01:25.301449	tech1@gmao.com	25632554	Karim Sassi	$2a$10$Ubljmy05tOEKrdKGDVOF/eCfqlEbQa6dAvGZQP2yUlELtyU3W1i3m	\N	TECHNICIAN	f	\N	\N	\N	\N	\N	\N	f
+2	456 Factory St, Sfax	2026-03-23 20:01:25.300351	responsable@gmao.com	12355688	Mohamed Ali	$2a$10$HYmqTuzwLdO3.QOOrYSSuOUneBo6dfvc89uVdJMTo4ETHLNoZpKsq	\N	RESPONSABLE	f	\N	\N	\N	\N	\N	\N	f
+1	123 Industrial Zone, Tunis	2026-03-23 20:01:25.277697	admin@gmao.com	11254552	Admin GMAO	$2a$10$Qe82xiI/LDL3DLmFxrkrG.KHX6LMKmtm86bhqF6apuRf0sCx//VAa	\N	ADMIN	f	\N	\N	\N	\N	\N	\N	f
 \.
 
 
@@ -302,35 +293,35 @@ COPY public.users (id, address, created_at, email, id_number, name, password, pr
 -- Name: machine_data_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.machine_data_id_seq', 8, true);
+SELECT pg_catalog.setval('public.machine_data_id_seq', 11, true);
 
 
 --
 -- Name: machines_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.machines_id_seq', 5, true);
+SELECT pg_catalog.setval('public.machines_id_seq', 10, true);
 
 
 --
 -- Name: task_history_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.task_history_id_seq', 2, true);
+SELECT pg_catalog.setval('public.task_history_id_seq', 1, false);
 
 
 --
 -- Name: tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.tasks_id_seq', 5, true);
+SELECT pg_catalog.setval('public.tasks_id_seq', 18, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 4, true);
+SELECT pg_catalog.setval('public.users_id_seq', 7, true);
 
 
 --
@@ -371,6 +362,14 @@ ALTER TABLE ONLY public.tasks
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT uk_6dotkott2kjsp8vw4d0m25fb7 UNIQUE (email);
+
+
+--
+-- Name: users uk_6km2m9i3vjuy36rnvkgj1l61s; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT uk_6km2m9i3vjuy36rnvkgj1l61s UNIQUE (uuid);
 
 
 --
@@ -422,8 +421,16 @@ ALTER TABLE ONLY public.task_history
 
 
 --
+-- Name: tasks fkmeg3m9hk7eyq7u5kpot87f9ey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.tasks
+    ADD CONSTRAINT fkmeg3m9hk7eyq7u5kpot87f9ey FOREIGN KEY (created_by_id) REFERENCES public.users(id);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
-\unrestrict AzD15abBh0htDkEoopO6EohD7u5KCVLEsI1YAlKx7YonpLTOhptdYmeQJWxg3tS
+\unrestrict OJHOqmueXdssILR9V2eBIUdXiA96FR4KAwH0v6FtjJKh9AreVe1BbBBbLIGbrmn
 
